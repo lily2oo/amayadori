a7b3406404843f9d41eb1f52df11e18f893fc80e
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-06-07 21:33:26 +0900

    スクロールイベント

--- js/index.js
+++ js/index.js
@@ -4,14 +4,29 @@ $(function () {
     });
 });
 
-$(function () {
-    var breakPoint = 768;
-    var event = $(window).innerWidth() > breakPoint ? 'mouseover' : 'touchstart';
-    $("#pagepiling div:nth-child(n+1):nth-child(-n+7)").on(event, function () {
+
+// Create a closure
+(function(){
+    // Your base, I'm in it!
+    var originalAddClassMethod = jQuery.fn.addClass;
+
+    jQuery.fn.addClass = function(){
+        // Execute the original method.
+        var result = originalAddClassMethod.apply( this, arguments );
+
+        // trigger a custom event
+        jQuery(this).trigger('cssClassChanged');
+
+        // return the original result
+        return result;
+    }
+})();
+
+// document ready function
+$(function(){
+    $(".section").bind('cssClassChanged', function(){ 
         $(".active").animate({
-                opacity: 1
-            },
-            1500
-        );
+            "opacity":"1"
+        },1500);
     });
 });

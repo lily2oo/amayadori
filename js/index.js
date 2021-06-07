33c4f5ae062a941b9ef16df03ead5b24fe359a4a
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-06-07 17:09:10 +0900

    遷移アニメーション試し

--- js/index.js
+++ js/index.js
@@ -3,3 +3,12 @@ $(function(){
             "background-color":"transparent",
         });
 });
+$(function(){
+    $("#pagepiling div:nth-child(n+2):nth-child(-n+7)").on("mouseover", function(){
+        $(this).animate({
+            opacity:1
+        },
+        1500
+        );
+    });
+});

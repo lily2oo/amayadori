ed38da4899eaeb8610ebf9be1b753c9bb6272827
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-06-07 18:27:32 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-$(function(){
-        $("#fv").css({
-            "background-color":"transparent",
-        });
+$(function () {
+    $("#fv").css({
+        "background-color": "transparent",
+    });
 });
-$(function(){
-    $("#pagepiling div:nth-child(n+2):nth-child(-n+7)").on("mouseover", function(){
-        $(this).animate({
-            opacity:1
-        },
-        1500
+
+$(function () {
+    $("#pagepiling div:nth-child(n+2):nth-child(-n+7)").on("mouseover", function () {
+        $(".active").animate({
+                opacity: 1
+            },
+            1500
         );
     });
 });

ce238de7983e12a9e75dc5408c7708c71d236456
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-06-10 22:03:55 +0900

    大幅アプデ

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ $(function () {
 
 
 // Create a closure
-(function(){
+(function () {
     // Your base, I'm in it!
     var originalAddClassMethod = jQuery.fn.addClass;
 
-    jQuery.fn.addClass = function(){
+    jQuery.fn.addClass = function () {
         // Execute the original method.
-        var result = originalAddClassMethod.apply( this, arguments );
+        var result = originalAddClassMethod.apply(this, arguments);
 
         // trigger a custom event
         jQuery(this).trigger('cssClassChanged');
@@ -23,10 +23,24 @@ $(function () {
 })();
 
 // document ready function
-$(function(){
-    $(".section").bind('cssClassChanged', function(){ 
+$(function () {
+    $(".section").bind('cssClassChanged', function () {
         $(".active").animate({
-            "opacity":"1"
-        },1500);
+            "opacity": "1"
+        }, 1500)
+
+        $('.active .tlt').textillate({
+            loop: true, //ループ
+            minDisplayTime: 3000, //テキストが置き換えられるまでの時間//
+            initialDelay: 300,         //遅延時間
+            // フェードイン
+            in: {
+                effect: 'fadeIn', //アニメーション効果、エフェクトの種類
+                delayScale: 1.5, //遅延時間の指定
+                delay: 45, //文字ごとの遅延時間
+                sync: false, //『true』全ての文字に同時に適応される
+                shuffle: false //ランダム
+            }
+        });
     });
 });

30bfba29c89a35c54df2d01e112c39e87d846e74
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-06-16 17:38:04 +0900

    ddd

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(window).on('load',function(){
     var loading = function(){
       $('.load').fadeOut('fast');
-      $('#pagepiling').fadeIn('slow');
+      $('#wrap').fadeIn('slow');
     };
     setTimeout(loading,4000); // setTimeoutを使って表示時間を設定する
   });
